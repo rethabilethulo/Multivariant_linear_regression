933e0ebe0972d88a2f69a870c6a87bb440094f6b
recruit <rethabilethulo>
2020-05-14 08:32:53 +0200

    deleted a file

--- OOP_for_Data_Science/ErrorCalculator.py
+++ OOP_for_Data_Science/ErrorCalculator.py
@@ -58,3 +58,44 @@ class ErrorCalculator:
         print(f"The maximum standard residuals = {max(std_resids)}")
         print(f"The Mean Square Error = {self.get_mse()}")
         print(f"The Root Square Mean Error = {self.get_rmse()}")
+
+
+#Plotter class to run claculations
+class Plotter():
+    
+    def __init__(self, y, y_predictor):
+        self.y = y
+        self.y_predictor = y_predictor
+
+    def test_calculations(self):
+        return self.y - self.y_predictor
+
+    def plot(self):
+        plt.hist(self.run_calculations())
+        plt.title("Model Prediction Residuals")
+        plt.xlabel("Residuals")
+        plt.ylabel("Frequency")
+        plt.show()
+
+
+##Histogram plotter
+class HistogramPlotter(Plotter):
+
+    def __init__(self, y, y_predictor):
+        super().__init__(y, y_predictor)
+
+
+
+#Scatterplot plotter
+class ScatterPlotter(Plotter):
+    
+    def __init__(self, y, y_predictor):
+        super().__init__(y, y_predictor)
+     
+def plot(self):
+        df = pd.DataFrame({"y":self.y, "y_prediction":self.y_prediction})
+        plot.scatter(data = df, x = "y", y = "y_predictions", color = "indigo")
+        plt.title("Model Predictions vs Actual Values")
+        plt.xlabel("Actual Values")
+        plt.ylabel("Prediction")
+        plt.show()
